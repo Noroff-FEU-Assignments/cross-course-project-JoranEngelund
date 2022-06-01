8ac04a8677bece5b954759ee7fd3e1b771549bf5
JoranEngelund <joranengelund@hotmail.com>
2022-06-01 15:57:19 +0200

    updated links between pages

---
+++ js/register.js
@@ -0,0 +1,7 @@
+const firstName = document.querySelector("#firstname");
+const surname = document.querySelector("#surname");
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
+const nameError = document.querySelector(".nameError");
+const emailError = document.querySelector(".emailError");
+const passwordError = document.querySelector(".passwordError");
